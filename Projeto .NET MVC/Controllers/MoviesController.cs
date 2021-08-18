4d5d45df8137cb5f8456a3eeb8df4815325c4515
William <william.bueno.rocha@gmail.com>
2021-08-18 17:54:57 -0300

    Primeiro model

---
+++ Projeto .NET MVC/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using Projeto.NET_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto.NET_MVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Sherek" };
+            return View(movie);
+        }
+    }
+}

b55d3c92ef5b8938db9a1867bb9fa7cf85486d19
William <william.bueno.rocha@gmail.com>
2021-08-18 21:10:18 -0300

    Testando as default routes

--- Projeto .NET MVC/Controllers/MoviesController.cs
+++ Projeto .NET MVC/Controllers/MoviesController.cs
@@ -14,6 +14,27 @@ namespace Projeto.NET_MVC.Controllers
         {
             var movie = new Movie() { Name = "Sherek" };
             return View(movie);
+            //return Content("Hello world!");
+            //return Json(movie, JsonRequestBehavior.AllowGet);
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
